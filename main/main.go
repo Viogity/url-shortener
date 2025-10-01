be706e521ad12d009736dba549b9efb8a3f687c0
Alexander <Dmitruk.sa@yandex.ru>
2025-10-01 15:13:29 +0300

    main.go URL sart_2

--- main/main.go
+++ main/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	_ "fmt"
 	_ "os"
 	_ "strings"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	fmt.Print("Hello, Enter your URL: ")
-	var longUrl string = "url"
-	fmt.Scanln(longUrl)
+	var longUrl string
+	fmt.Scanln(&longUrl)
 	fmt.Println(longUrl)
+
+	connStr := "user=postgres password=mypass dbname=productdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	result, err := db.Exec("insert into Products (model, company, price) values ('iPhone X', $1, $2)",
+		"Apple", 72000)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.LastInsertId()) // не поддерживается
+	fmt.Println(result.RowsAffected()) // количество добавленных строк
 }
